04917d087a7bcd97ff8f4e49432165405eb4bdb6
pramodaya <pramodayajayalath@gmail.com>
2020-03-21 22:27:22 +0530

    code added

---
+++ TrueMarbleData/TrueMarbleData/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrueMarbleData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host;
+            NetTcpBinding tcpBinding = new NetTcpBinding();
+
+            tcpBinding.MaxReceivedMessageSize = System.Int32.MaxValue;
+            tcpBinding.ReaderQuotas.MaxArrayLength = System.Int32.MaxValue;
+
+            host = new ServiceHost(typeof(TMDataControllerImpl));
+            host.AddServiceEndpoint(typeof(ITMDataController), tcpBinding, "net.tcp://localhost:50001/TMData");
+
+            host.Open();
+
+            Console.WriteLine("press enter to exit");
+            Console.ReadLine();
+
+            host.Close();
+
+        }
+    }
+}
---
+++ TrueMarbleData/TrueMarbleData/TMDataControllerImpl.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrueMarbleData
+{
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, UseSynchronizationContext = false)]
+    public class TMDataControllerImpl : ITMDataController
+    {
+        public int GetNumTilesAcross(int zoom)
+        {
+            int tileX = 0;
+            int tileY;
+            int result;
+
+            try
+            {
+                result = TMDLLWrapper.GetNumTiles(zoom, out tileX, out tileY);
+
+                if (result == 0)
+                {
+                    throw new Exception(" retreving error");
+                }
+                return tileX;
+            }
+            catch (System.DllNotFoundException)
+            {
+                Console.WriteLine("Dll Not Found");
+                return 0;
+            }
+
+        }
+
+        public int GetNumTilesDown(int zoom)
+        {
+            int tileX = 0;
+            int tileY;
+            int result;
+
+            try
+            {
+                result = TMDLLWrapper.GetNumTiles(zoom, out tileX, out tileY);
+
+                if (result == 0)
+                {
+                    throw new Exception(" retreving error");
+                }
+                return tileY;
+            }
+            catch (System.DllNotFoundException)
+            {
+                Console.WriteLine("Dll Not Found");
+                return 0;
+            }
+        }
+
+        public int GetTileHeight()
+        {
+            int width;
+            int height;
+            int result;
+            try
+            {
+                result = TMDLLWrapper.GetTileSize(out width, out height);
+                if (result == 0)
+                {
+                    throw new Exception("retreving height error");
+                }
+                return height;
+            }
+            catch (System.DllNotFoundException)
+            {
+                Console.WriteLine("Dill Not Found");
+                return 0;
+            }
+
+        }
+
+        public int GetTileWidth()
+        {
+            int width;
+            int height;
+            int result;
+            try
+            {
+                result = TMDLLWrapper.GetTileSize(out width, out height);
+                if (result == 0)
+                {
+                    throw new Exception("retreving width error");
+                }
+                return width;
+            }
+            catch (System.DllNotFoundException)
+            {
+                Console.WriteLine("Dill Not Found");
+                return 0;
+            }
+        }
+
+        public byte[] LoadTile(int zoom, int x, int y)
+        {
+            try
+            {
+                int width;
+                int height;
+                int jpgSize;
+                int sizeRes;
+                int result;
+
+                sizeRes = TMDLLWrapper.GetTileSize(out width, out height);
+                byte[] buff = new byte[width * height * 3];
+
+                result = TMDLLWrapper.GetTileImageAsRawJPG(zoom, x, y, out buff, width * height * 3, out jpgSize);
+
+                if (result == 1)
+                {
+                    return buff;
+                }
+
+                else
+                {
+                    return null;
+                }
+
+
+            }
+            catch (System.DllNotFoundException)
+            {
+                Console.WriteLine("Dill Not Found");
+                return null;
+
+            }
+
+
+        }
+    }
+}
